05e120a46863d69f43a513dc3c591d4efcc85601
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:15:41 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan2-Deklarasi & Pengaksesan Variabel/src/pbo1/pkg10118006/latihan2/deklarasi/pengaksesan/variabel/PBO110118006Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan2.deklarasi.pengaksesan.variabel;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk menampilkan Deklarasi & Pengaksesan Variabel
+ */
+public class PBO110118006Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    //Deklarasi Variabel
+    int nilaiInt;
+    final double PHI = 3.14 ; //konstanta uppercase
+    boolean nilailogika;
+    char nilaikarakter;
+    
+    //Memberi nilai ke variabel
+    nilaiInt = 30;
+    nilailogika = false;
+    nilaikarakter = 'd';
+    
+    //Menampilkan hasil
+    System.out.println();
+    System.out.println("Isi variabel nilai="+nilaiInt);
+    System.out.println("Isi variabel PHI="+PHI);
+    System.out.println("Isi variabel logika="+nilailogika);
+    System.out.println("Isi variabel="+nilaikarakter);
+    
+    }
+    
+}
